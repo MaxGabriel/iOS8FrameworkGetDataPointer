102066d2e62bc102921f4989b970c256c7d3e1af
Maximilian Tagher <feedback.tagher@gmail.com>
2015-03-19 16:47:52 -0700

    Initial Commit

---
+++ ListFrameworks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ListFrameworks
+//
+//  Created by Maximilian Tagher on 3/19/15.
+//  Copyright (c) 2015 Tagher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ListFrameworksTests/ListFrameworksTests.m
@@ -0,0 +1,40 @@
+//
+//  ListFrameworksTests.m
+//  ListFrameworksTests
+//
+//  Created by Maximilian Tagher on 3/19/15.
+//  Copyright (c) 2015 Tagher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ListFrameworksTests : XCTestCase
+
+@end
+
+@implementation ListFrameworksTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
